9fb4a97b745ba3f02ae62e006c9c3da7bcca4b48
reivincitot <reivincitot@gmail.com>
2023-09-13 17:29:18 -0300

    Inicio Pregunta 5 del modulo 4

--- lab-4.py
+++ lab-4.py
@@ -10,8 +10,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
-from sklearn.linear_model import LinearRegression
 import seaborn as sns
+
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 filename = "D:/Curso ibm/Data analysis with python/automobileEDA.csv"
 df = pd.read_csv(filename,header=0)
@@ -158,16 +162,152 @@ print(lm2.coef_)
 
 width = 12
 height = 10
+
 plt.figure(figsize=(width,height))
 sns.regplot(x="highway-mpg", y="price", data=df)
 plt.ylim(0,)
 plt.show()
 
-# Podemos ver desde este entramado que el precio tiene una correlación negativa con respecto a highway-mpg dado que la pendiente de regresión es negativa. Una cosa para mantener en mente cuando miramos el entramado de regresión es mirar cuan dispersos se encuentran los puntos de entrada con respecto a la linea de regresión, esto te dará una buena indicacion de la varianza de la información y si un modelo sera el mas adecuado o no. si la información esta demasiado alejada de la linea de regresión, este modelo puede no ser el mejor para esta información
+# Podemos ver desde este entramado que el precio tiene una correlación negativa con respecto a highway-mpg dado que la pendiente de regresión es negativa. Una cosa para mantener en mente cuando miramos el entramado de regresión es mirar cuan dispersos se encuentran los puntos de entrada con respecto a la linea de regresión, esto te dará una buena indicación de la varianza de la información y si un modelo sera el mas adecuado o no. si la información esta demasiado alejada de la linea de regresión, este modelo puede no ser el mejor para esta información
 
 # Vamos a comparar esto entramado con el entramado de "peak-rpm"
 
 plt.figure(figsize=(width,height))
 sns.regplot(x="peak-rpm", y="price", data=df)
 plt.ylim(0,)
-plt.show
+plt.show()
+
+# Pregunta 3 Dado el entramado de regresión de arriba, es "peak-rpm" o "highway-mpg" mas fuertemente correlacionado con "price"?. usa el método .corr() para verificar la respuesta
+
+df[["peak-rpm","highway-mpg","price"]].corr()
+
+# Residual plot una buena manera de visualizar la variación de la información es usar un residual plot
+# Que es residual? las diferencias entre el valor observado (Y) y el valor predecido (Yhat) es llamado residual(e), cuando miramos al regression plot, el residuo es la distancia desde el punto de entrada y la linea de regresión
+
+# Así que ¿que es un residual plot? Un residual plot es un gráfico de residuos en el eje vertical Y la variable independiente en el eje horizontal X
+# A que le prestamos atención al mirar un residual plot. Miramos a la dispersion de los residuos
+
+# si el punto en un plot residual están dispersos al azar al rededor del eje X entonces el modelo linear es el apropiado par esta información
+# Por que es eso? Esparcidos al azar significa que la varianza es constante, y por lo tanto el modelo lineal esta bien ajustado para esta información
+
+width = 12
+height = 10
+plt.figure(figsize=(width, height))
+sns.residplot(x="highway-mpg",y="price",data=df)
+plt.show()
+
+# Que nos muestra esta trama podemos ver que los datos de entrada no están dispersos de forma aleatoria al rededor del eje x, llevándonos a pensar, que un modelo no lineal seria mas apropiado para esta información
+
+# Multiple linear regression
+
+# Como visualizamos un modelo para una regresión lineal multiple? esto se vuelve un poco mas complicado por que no puedes visualizar con regresión o residual plot
+
+# Una manera de hacerlo es mirar el ajuste del modelo mirando el entramado de distribución y comparando con la distribución de los valores verdaderos
+
+# Primero haremos una predicción
+Z = df[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg']]
+
+Y_hat=lm.predict(Z)
+
+plt.figure(figsize=(width,height))
+
+ax1= sns.kdeplot(df['price'],color="r",label="Actual value", fill=False)
+sns.kdeplot(Y_hat, color="b", label="Fitted values", ax= ax1, fill=False)
+
+plt.title("Actual values vs Fitted values for price")
+plt.xlabel("Price (in dollars)")
+plt.ylabel("Proportion of Cars")
+
+plt.show()
+plt.close
+
+# Regresión polinómica (Polynomial Regression) es un caso particular de la generalidad de la regresión lineal o regresión lineal multiple
+# Obtenemos relaciones no lineales al elevar al cuadrado o establecer términos de orden superior de las variables predictoras.
+# hay diferentes ordenes en la regresión de polinomios:
+# Cuadrática - segundo orden: Yhat = a + b1X + b2X**2
+# Cubico - tercer orden: Yhat = a + b1X +b2X**2 + b3X**3
+# Alto orden: Y= a+b1x**2 + b2x**2 + b3**3
+
+# Vimos anteriormente que un modelo lineal no provee el resultado mas ajustado mientras se usa "highway-mpg" como variable predictora. Veremos si podemos ajustar el modelo de polinomios a la información
+
+# Usaremos la siguiente función:
+
+def PlotPolly(model,independent_variable, dependent_variable, Name):
+    x_new = np.linspace(15,55,100)
+    y_new = model(x_new)
+
+    plt.plot(independent_variable, dependent_variable,'.',x_new,y_new,'-')
+    plt.title('Polynomial Fit with Matplotlib for Price ~ Length')
+    ax = plt.gca()
+    ax.set_facecolor((0.898,0.898,0.898))
+    fig = plt.gcf()
+    plt.xlabel(Name)
+    plt.ylabel('Price of Cars')
+
+    plt.show()
+    plt.close()
+
+# Vamos a tomar las variables
+x = df['highway-mpg']
+y = df['price']
+
+# Vamos a ajustar el polinomio usando la función polyfit, después usa la función poly1d para:
+
+f = np.polyfit(x,y,3)
+p = np.poly1d(f)
+print(p,"\n")
+
+# Vamos a entramar la función
+PlotPolly(p,x,y,'highway-mpg')
+
+# Pregunta 4 crea 11 modelos polynomial ordenados con la variable "x" y "y" de arriba.
+
+f1 = np.polyfit(x,y,11)
+p1 = np.poly1d(f1)
+print(p1,"\n")
+PlotPolly(p1,x,y,'Highway MPG')
+
+# La expresión analítica para una función polinómica multivariada se vuelve complicada. Por ejemplo, la expresión para un polinomio de segundo orden (grado=2) con dos variables se da por: Yhat = a + b_1X_1 + b_2X_2 + b_3X_1X_2 + b_4X_1**2 + b_5X_2**2
+
+# Podemos crear un objeto de caracteristicas polinomiales de grado 2:
+
+pr= PolynomialFeatures(degree=2)
+print(pr,"\n")
+
+Z_pr = pr.fit_transform(Z)
+
+print(Z.shape,"\n")
+
+print(Z_pr.shape,"\n")
+
+# Pipeline o secuencia de pasos: data pipelines simplifican los pasos del procesado de la información. Nosotros usamos el modulo Pipeline para crear un pipeline. También usamos StandardScaler como un paso en nuestra pipeline from sklearn.pipeline import Pipeline from sklearn.preprocessing import StandardScaler
+
+# Creamos el pipeline comenzando por crear una lista de tuplas incluyendo los nombres del modelo o el estimador y su correspondiente constructor
+
+Input = [('scale',StandardScaler()),('polynomial', PolynomialFeatures(include_bias=False)),('model',LinearRegression())]
+
+# Ingresamos la lista como un argumento al constructor del pipeline
+
+pipe = Pipeline(Input)
+print(pipe,"\n")
+
+# Primero, convertimos la información de tipo Z a tipo float para evitar las alertas de conversión que pueden aparecer como resultados de StandardScaler tomando float inputs. Entonces , podemos normalizar la información, para realizar una transformación y ajuste del modelo en simultaneo.
+
+Z = Z.astype(float)
+print(pipe.fit(Z,y),"\n")
+
+# Similarmente, podemos normalizar la información, realizaremos una transformación y una predicción en simultaneo
+
+ypipe = pipe.predict(Z)
+print(ypipe[0:4])
+
+# Pregunta 5 Crea un pipeline que estandarice la información , entonces produce una predicción usando una regresión lineal usando la característica Z y el objetivo y.
+
+Input = [('Scale',StandardScaler()),('model', LinearRegression())]
+
+pipe = Pipeline(Input)
+
+pipe.fit(Z,y)
+
+ypipe = pipe.predict(Z)
+print(ypipe[0:10])
