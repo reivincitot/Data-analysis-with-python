bdc6e3d7da576a5ba82d03397ab3ce7364789c86
reivincitot <reivincitot@gmail.com>
2023-09-21 09:58:01 -0300

    Pregunta 2 lab 5

--- lab-5.py
+++ lab-5.py
@@ -10,8 +10,15 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 from ipywidgets import interact, interactive,fixed,interact_manual
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+
+
+
+
 path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/module_5_auto.csv'
 df = pd.read_csv(path)
 df.to_csv("module_5_auto.csv")
@@ -77,7 +84,33 @@ print("Numero de muestras de entrenamiento: ",x_train.shape[0],"\n")
 # Usa la función "train_test_split" para dividir el dataset para que el 40% de las muestras sean utilizadas para pruebas, prepara los parámetros "random_state" igual a zero. la salida de la función debe ser la siguiente: "x_train1","x_test1", "y_train1","y_test1".
 
 print("Pregunta 1\nUsa la función \"train_test_split\" para dividir el dataset para que el 40% de las muestras sean utilizadas para pruebas, prepara los parámetro \"random_state\" igual a zero. la salida de la función debe ser la siguiente: \"x_train1\",\"x_test1\", \"y_train1\",\"y_test1\".\n")
-x_train1,x_test1,y_train1,y_test1 = train_test_split(x_data,y_data,test_size=0.40,random_state=0)
+x_train1,x_test1,y_train1,y_test1 = train_test_split(x_data, y_data,test_size=0.4 , random_state=0)
 
 print("Numero de muestras de prueba: ", x_test1.shape[0])
 print("Numero de muestras de entrenamiento:",x_train1.shape[0],"\n")
+
+# Vamos a importar desde el modulo linear_regression LinearRegression
+# From sklearn.linear_model import LinearRegression
+# Creamos el objeto Linear Regression
+lre=LinearRegression()
+
+# Ajustamos el modelo usando la característica "horsepower":
+lre.fit(x_train[['horsepower']],y_train)
+
+# Vamos a calcular el R^2 en la información de prueba:
+
+x = lre.score(x_test[['horsepower']],y_test)
+print(x)
+# Podemos ver que el R^2 es mas pequeño usando la información de prueba comparándola con la información de entrenamiento
+
+y = lre.score(x_train[['horsepower']],y_train)
+print(y)
+
+#-----------------------Pregunta 2
+# Encuentra el R^2 en el dataset usando el 40% del data set para pruebas
+x_train2,x_test2,y_train2,y_test2 = train_test_split(x_data,y_data, test_size=0.4, random_state=0)
+
+x = lre.fit(x_train2[['horsepower']],y_train2)
+y = lre.score(x_test2[['horsepower']],y_test2)
+print(x)
+print(y)
